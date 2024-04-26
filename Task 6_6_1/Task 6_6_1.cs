ca353603e98be2290ce415775589a0a74b43062d
Ilia <isinetov@mail.ru>
2024-04-26 18:41:27 +0400

    Задание 6.6.1 Напишите класс светофор (TrafficLight) с 2 методами:
    Закрытый метод ChangeColor, принимающий 1 строковый параметр color.
    Открытый метод GetColor, который не принимает параметры, но выдает строковое значение цвета.
    
    Методы реализовывать не нужно.

---
+++ Task 6_6_1/Task 6_6_1.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_6_6_1 
+{
+    class TrafficLight 
+    {
+        private void ChangeColor(string color)
+        {
+
+        }
+
+        public string GetColor()
+        {
+            
+        }
+    }
+}
