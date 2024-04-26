81b84e4e9d16ab2c6ce21645ecc09d94db116b0f
Ilia <isinetov@mail.ru>
2024-04-26 15:49:49 +0400

    Задание 6.5.2. Опишите классы C# для следующих геометрических объектов: треугольник, круг, квадрат.
    В классах нужно объявить поля для длин сторон (радиус) и методы для получения площади и периметра (длины окружности) без их реализации и без конструкторов.

---
+++ Task 6_5_2/triangle.cs
@@ -0,0 +1,44 @@
+﻿class Triangle 
+{
+    int a, b, c;
+
+    public void FindPerimeter() 
+    {
+
+    }
+
+    public void FindSquare() 
+    {
+
+    }
+}
+
+class Circle 
+{
+    double radius;
+
+    public void FindCircleLength() 
+    {
+
+    }
+
+    public void FindSquare() 
+    {
+
+    }
+}
+
+class Square 
+{
+    int a;
+
+    public void FindSquare() 
+    {
+
+    }
+
+    public void FindPerimeter() 
+    { 
+
+    }
+}
