6b9863ea805cb37ad41988702bcd1db71689f08b
Ilia <isinetov@mail.ru>
2024-04-26 14:59:36 +0400

    Задание 6.3.1. Напишите такой код, который бы при типе компании, равному типу "Банк", и городе "Санкт-Петербург" выводил в консоль сообщение "У банка ??? есть отделение в Санкт-Петербурге", где вместо "???" выводилось бы название компании.
    Если у компании нет названия, вместо него должно быть "Неизвестная компания".

---
+++ Task 6_3_1/Task 6_3_1.cs
@@ -0,0 +1,36 @@
+﻿class Company
+{
+    public string Type;
+    public string Name;
+}
+
+class Department
+{
+    public Company Company;
+    public City City;
+}
+
+class City
+{
+    public string Name;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var company = new Company { Type = "Банк", Name = null };
+        var city = new City { Name = "Санкт-Петербург" };
+        var department = GetCurrentDepartment(company, city);
+    }
+
+    static Department GetCurrentDepartment(Company company, City city)
+    {
+        if (company.Type == "Банк" && city.Name == "Санкт-Петербург") 
+        {
+            Console.WriteLine($"У банка {company?.Name ?? "Неизвестная компания"} есть отделение в Санкт-Петербурге.");
+        }
+        Department department = new Department {Company = company, City = city };
+        return department;
+    }
+}
