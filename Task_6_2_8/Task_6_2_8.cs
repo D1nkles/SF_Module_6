0e3c15fc80231687bc4b54bb428b7fefe0fa95e9
Ilia <isinetov@mail.ru>
2024-04-26 14:08:52 +0400

    Задание 6.2.8. Создайте класс Rectangle для представления прямоугольников удовлетворяющий следующим требованиям:
    1. Класс должен содержать целочисленные поля для сторон a и b.
    2. Класс должен содержать метод Square, возвращающий площадь прямоугольника (произведение сторон).
    3. Класс должен содержать 3 конструктора: с 2 параметрами, когда a != b, с 1 параметром, когда a == b, и конструктор без параметров по умолчанию, в котором стороны будут заполняться как a = 6, b = 4.

---
+++ Task_6_2_8/Task_6_2_8.cs
@@ -0,0 +1,27 @@
+﻿class Rectangle 
+{
+    public uint a, b;
+
+    public Rectangle() 
+    {
+        a = 6;
+        b = 4;
+    }
+
+    public Rectangle(uint  num) 
+    {
+        a = num;
+        b = num;
+    }
+
+    public Rectangle(uint num1, uint num2) 
+    {
+        a = num1;
+        b = num2;
+    }
+
+    public uint Square() 
+    {
+        return a * b;
+    }
+}
