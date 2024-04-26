3d7437e94a0766cab6a6cc7abbeb9bd58a6f32c2
Ilia <isinetov@mail.ru>
2024-04-26 15:32:48 +0400

    Задание 6.3.2. Для класса Bus реализуйте метод PrintStatus, который будет сообщать о количестве пассажиров в автобусе, если они есть. Или сообщать, что автобус пуст.

---
+++ Task 6_3_2/Task 6_3_2.cs
@@ -0,0 +1,24 @@
+﻿class Bus
+{
+    public int? Load;
+    public void PrintStatus()
+    {
+        if (Load > 0) 
+        {
+            Console.WriteLine($"В автобусе {Load} пассажиров.");
+        }
+        else 
+        {
+            Console.WriteLine("В автобус пуст.");
+        }
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        var bus = new Bus() { Load = 5 };
+        bus.PrintStatus();
+    }
+}
