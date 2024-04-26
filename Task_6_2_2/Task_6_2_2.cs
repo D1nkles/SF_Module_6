e8f5a88322b5375614e124ebeae6e3977a72bc0a
Ilia <isinetov@mail.ru>
2024-04-26 13:50:10 +0400

    Добавьте файлы проекта.

---
+++ Task_6_2_2/Task_6_2_2.cs
@@ -0,0 +1,24 @@
+﻿class Pen
+{
+    public string color;
+    public int cost;
+    public Pen()
+    {
+        color = "Черный";
+        cost = 100;
+    }
+
+    public Pen(string penColor, int penCost)
+    {
+        color = penColor;
+        cost = penCost;
+    }
+}
+
+class Task_6_2_2 
+{
+    static void Main(string[] args) 
+    {
+        
+    }
+}
